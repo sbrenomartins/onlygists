14c75bca3b02f9f977c517d1681cd86f4c39499e
Breno M. Silva <sbrenomartins@gmail.com>
2024-10-19 18:04:09 -0300

    chore: setup project

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-red-300 font-[Inter]">onlygists</h1>
+
+    <Button label="Submit" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@primevue/nuxt-module'],
+  css: ['primeicons/primeicons.css'],
+  googleFonts: {
+    base64: true,
+    fontsDir: 'assets/fonts',
+    overwriting: true,
+    families: {
+      Inter: [300, 500, 800]
+    }
+  },
+  primevue: {
+    options: { unstyled: true },
+    importPT: {
+      as: 'lara',
+      from: path.resolve(__dirname, './assets/presets/lara/')
+    }
+  }
+})
