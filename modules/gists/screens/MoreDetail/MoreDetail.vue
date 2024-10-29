f239e56b21c753c383cab7f62dbfa607d826dd03
Breno M. Silva <breno.martins@supermix.com.br>
2024-10-29 15:49:42 -0300

    feat: create the page for the gist details

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,3 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
+</script>
 
-<template>detalhes do gist</template>
+<template>
+  <PublicHeadline
+    title="currency_format.py"
+    lang="python"
+    description="função em python para fazer **formatação de moeda**"
+    author="sbrenomartins"
+  />
+</template>
