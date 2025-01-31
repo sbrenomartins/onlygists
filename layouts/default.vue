3ae20defee8cf8f300c89fe66f439ee06a546a30
Breno M. Silva <sbrenomartins@gmail.com>
2025-01-31 14:43:13 -0300

    feat: config default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,5 @@
 <template>
-  <slot />
+  <div class="w-full h-full flex flex-col items-center">
+    <slot />
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import Home from '@/modules/landing-page/screens/Home/Home.vue';
+
+definePageMeta({
+  layout: 'default', // Comportamento padrão do Nuxt, porém podemos definir um layout específico para cada tela
+});
 </script>
 
 <template>
