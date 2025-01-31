b26c73b2a972ecabd557f12d32d06f9a1392b2e1
Breno M. Silva <sbrenomartins@gmail.com>
2025-01-31 12:00:07 -0300

    feat: first module of landing page and initializing layouts

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-red-300 font-[Inter]">onlygists</h1>
-    <Button label="Submit" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
