0d260facfdcfb5a4c54f9c350bea29191170aa41
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-12 20:36:19 -0300

    feat: modulo de reports e components WidgetGroup e WidgetCondensed

---
+++ modules/reports/components/Widget/Group/Group.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col md:flex-row justify-between gap-5 py-10">
+    <slot />
+  </div>
+</template>
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue';
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue';
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue';
 </script>
 
 <template>
   <PublicHeadline />
+
+  <WidgetGroup>
+    <WidgetCondensed :value="10" label="Total de gists" />
+    <WidgetCondensed :value="5" label="Gists gratuitos" />
+    <WidgetCondensed :value="5" label="Gists pagos" />
+  </WidgetGroup>
 </template>
