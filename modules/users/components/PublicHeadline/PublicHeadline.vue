4b2e979c1a804d6d1de2b97fecd760f8ea194ae5
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-12 20:12:52 -0300

    feat: modulo de users e component public headline

---
+++ modules/users/components/PublicHeadline/PublicHeadline.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    avatarUrl: string;
+    bio?: string;
+    city?: string;
+    state?: string;
+  }>(),
+  {
+    name: 'Breno M. Silva',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/36958762?v=4',
+    bio: 'Software Engineer',
+    city: 'Belo Horizonte',
+    state: 'MG',
+  },
+);
+</script>
+
+<template>
+  <div class="w-full flex flex-col justify-center items-center gap-2">
+    <div class="w-24 md:w-32 rounded-full overflow-hidden">
+      <img :src="props.avatarUrl" alt="foto do usuário" class="w-full" />
+    </div>
+
+    <h1 class="font-[Inter] text-gray-700 text-center font-bold text-2xl md:text-3xl">{{ props.name }}</h1>
+    <p class="font-[Inter] text-gray-700 text-center font-regular md:text-lg" v-if="props.bio">{{ props.bio }}</p>
+
+    <p class="font-[Inter] font-regular text-gray-500 text-sm md:text-base" v-if="props.city && props.state">
+      <i class="pi pi-map-marker"></i>
+      {{ props.city }}, {{ props.state }}
+    </p>
+  </div>
+</template>
