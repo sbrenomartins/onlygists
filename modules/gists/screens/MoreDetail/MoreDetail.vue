5359374bdadd7bbd74bbb47e0f6d2234dff5fd5d
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-15 20:30:31 -0300

    feat: component DialogPaymentSuccess com defineModel

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
 import GistCodeSnippet from '@/modules/gists/components/CodeSnippet/CodeSnippet.vue';
+import DialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+
+const route = useRoute();
+const isPaymentSuccessfully = ref<boolean>(false);
+
+onMounted(() => {
+  const { success_payment } = route.query;
+
+  if (success_payment) {
+    isPaymentSuccessfully.value = true;
+  }
+});
 </script>
 
 <template>
   <PublicHeadline />
 
   <GistCodeSnippet />
+
+  <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
 </template>
