6d7c845bc11c8d2104370d45c39eee272c2ef9bb
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-15 18:42:07 -0300

    feat: criando hoc loader para o component GistCardGroup

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -4,7 +4,16 @@ import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue';
 import WidgetGroupLoader from '@/modules/reports/components/Widget/Group/Loader.vue';
 import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue';
 import GistCardGroup from '@/modules/gists/components/Card/Group/Group.vue';
+import GistCardGroupLoader from '@/modules/gists/components/Card/Group/Loader.vue';
 import GistCardItem from '@/modules/gists/components/Card/Item/Item.vue';
+
+const route = useRoute();
+const router = useRouter();
+
+const handleNavigateToDetail = (id: string) => {
+  const { username } = route.params;
+  router.push(`/${username}/gist/${id}`);
+};
 </script>
 
 <template>
@@ -20,10 +29,18 @@ import GistCardItem from '@/modules/gists/components/Card/Item/Item.vue';
 
   <WidgetDefault title="Todos os gists">
     <GistCardGroup>
-      <GistCardItem />
-      <GistCardItem />
-      <GistCardItem />
-      <GistCardItem />
+      <GistCardGroupLoader :loading="false">
+        <GistCardItem
+          @tap="handleNavigateToDetail"
+          v-for="n in 4"
+          key="n"
+          id="123"
+          title="useCurrentUser.ts"
+          description="Hook para controlar a **store** do usuário"
+          :price="10"
+          lang="typescript"
+        />
+      </GistCardGroupLoader>
     </GistCardGroup>
   </WidgetDefault>
 </template>
