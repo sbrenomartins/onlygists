3e57283b21eee69b686dcfe7b61a389e93a6bad9
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-15 20:41:00 -0300

    feat: component DialogPaymentError e LazyComponents

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
 import GistCodeSnippet from '@/modules/gists/components/CodeSnippet/CodeSnippet.vue';
-import DialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+import LazyDialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+import LazyDialogPaymentError from '@/modules/payments/components/DialogPaymentError/DialogPaymentError.vue';
 
 const route = useRoute();
 const isPaymentSuccessfully = ref<boolean>(false);
+const isPaymentFail = ref<boolean>(false);
 
 onMounted(() => {
-  const { success_payment } = route.query;
+  const { success_payment, fail_payment } = route.query;
 
   if (success_payment) {
     isPaymentSuccessfully.value = true;
+  }
+
+  if (fail_payment) {
+    isPaymentFail.value = true;
   }
 });
 </script>
@@ -20,5 +26,6 @@ onMounted(() => {
 
   <GistCodeSnippet />
 
-  <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentError v-model:visible="isPaymentFail" />
 </template>
