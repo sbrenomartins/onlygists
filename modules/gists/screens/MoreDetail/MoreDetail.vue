3672df518b8db73ab74725c17d3bd4516cdc9cb4
Breno M. Silva <sbrenomartins@gmail.com>
2025-02-15 19:23:42 -0300

    feat: criando tela de detalhe do gist e component PublicHeadline

---
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue';
+</script>
+
+<template>
+  <PublicHeadline />
+</template>
---
+++ pages/[username]/gist/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MoreDetail from '@/modules/gists/screens/MoreDetail/MoreDetail.vue';
+
+definePageMeta({
+  layout: 'default',
+});
+</script>
+
+<template>
+  <MoreDetail />
+</template>
